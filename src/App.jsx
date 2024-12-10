371042873bcf1b77052a016ef679c7161ec330f6
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2024-12-10 22:21:52 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom"; // Importez useLocation ici
 import HomePage from '../Pages/HomePage';
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import Navbar from '../Composants/Navbar';
-import Navbardroite from '../Composants/Navbardroite';
-import InscritpionPage from '../Pages/InscriptionPage';
-import ConnexionPage from '../Pages/ConnexionPage';
+import "./App.css";
+import Navbar from "../Composants/Navbar";
+import Navbardroite from "../Composants/Navbardroite";
+import DashboardAdmin from "../Pages/DashboardAdmin";
+import NavBarAdmin from "../Composants/NavbarAdmin";
+
+function Layout() {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* Affiche une Navbar différente selon la page */}
+      {location.pathname === "/" && (
+        <>
+          <Navbar />
+          <Navbardroite />
+        </>
+      )}
+      {location.pathname === "/DashboardAdmin" && <NavBarAdmin />}
+    </>
+  );
+}
 
 function App() {
   return (
