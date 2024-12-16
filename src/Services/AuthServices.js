50fe1a8bc5767307361643741e66f1b9ffd2cef9
gdelcroix <4097422+gdelcroix@users.noreply.github.com>
2024-12-16 16:36:45 +0100

    Login: - suppression d'un item d'état déconnecté, lors de la connection App: - execution du getUser dans le useEffect pour traitement à l'effet AuthService: - correction de isValid dans logout -affectation d'une variable a null a la déconnection Navbar: -link inutile retiré -modif de useEffect pour vérification de role si existant.

--- src/Services/AuthServices.js
+++ src/Services/AuthServices.js
@@ -48,6 +48,7 @@ function logout() {
   delete axios.defaults.headers['Authorization'];
   localStorage.removeItem('token');
   localStorage.setItem('loggedOut', 'true'); // Ajout d'un indicateur
+  isTokenValidCache = null;
   console.log('Logged out successfully');
 
   // Redirection au lieu de recharger
@@ -60,7 +61,6 @@ function isValid() {
   if (isTokenValidCache !== null) {
     return isTokenValidCache;
   }
-
   const token = localStorage.getItem('token');
   if (token) {
     const decodedToken = jwtDecode(token);
@@ -79,11 +79,7 @@ function isValid() {
     }
   } else {
     // Empêcher une déconnexion répétée
-    if (!localStorage.getItem('loggedOut')) {
-      logout();
-    }
-    isTokenValidCache = false;
-    return false;
+    return null;
   }
 }
 
