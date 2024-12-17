898a1b9a10502dcf85b60ac523a469512f168cac
gdelcroix <4097422+gdelcroix@users.noreply.github.com>
2024-12-17 18:06:50 +0100

    suppression redirection inexistante

--- Pages/Login.jsx
+++ Pages/Login.jsx
@@ -39,7 +39,7 @@ const Login = ({ setShowLoginModal }) => {
       } else {
         setErrorMessage("Erreur de connexion");
       }
-      navigate('/compte');
+      navigate('/');
     } catch (err) {
       console.error("erreur :",err.response.status, err.response.data.message);
       if (err.response.status === 404) {
